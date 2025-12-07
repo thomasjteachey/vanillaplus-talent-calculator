8188e2ab7f81194b7d8fdf7cf39e0536e7cf96e5
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2025-12-07 16:48:48 -0500

    Fix prerequisite arrow alignment

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -35,10 +35,23 @@ interface Props {
 export const Arrow: React.FC<Props> = ({ dir, from, to, active }) => {
   const arrowType = `${dir}${active ? "--gold" : ""}` as keyof typeof imageMap;
 
+  const posToCoords = (pos: Position) => ({
+    row: pos.charCodeAt(0) - 96,
+    col: Number(pos[1]),
+  });
+
+  const fromCoords = posToCoords(from);
+  const toCoords = posToCoords(to);
+
+  const rowStart = Math.min(fromCoords.row, toCoords.row);
+  const rowEnd = Math.max(fromCoords.row, toCoords.row) + 1;
+  const colStart = Math.min(fromCoords.col, toCoords.col);
+  const colEnd = Math.max(fromCoords.col, toCoords.col) + 1;
+
   return (
     <div
       className={`Arrow-container Arrow-container--${dir}`}
-      style={{ gridArea: `${from} / ${from} / ${to} / ${to}` }}
+      style={{ gridArea: `${rowStart} / ${colStart} / ${rowEnd} / ${colEnd}` }}
     >
       <div
         className={`Arrow Arrow--${dir}`}
